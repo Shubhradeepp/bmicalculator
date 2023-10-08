a8a0da62a37317c7482eae79b493860d91d4ef7a
Shubhradeep Maity <145332719+Shubhradeepp@users.noreply.github.com>
2023-10-08 18:14:50 +0530

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form');
+// const form = document.querySelector('.cal');
+form.addEventListener('submit',(e)=> {
+  // click in place of submit  if .cal
+  e.preventDefault();
+  console.log(e);
+  console.log(e.target);
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
+
